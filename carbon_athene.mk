1f42dc7d9f824b8912e5c918cebd9e345bd3163f
xenreon <souradeep999@gmail.com>
2017-06-24 13:07:11 +0530

    Carbon-ify

--- carbon_athene.mk
+++ carbon_athene.mk
@@ -13,10 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common carbon stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 $(call inherit-product, device/motorola/athene/full_athene.mk)
+
+# for specific
+$(call inherit-product, vendor/motorola/athene/athene-vendor.mk)
+
+# Inherit GSM Stuff
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -25,7 +32,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
-PRODUCT_NAME := lineage_athene
+PRODUCT_NAME := carbon_athene
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := athene
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo carbon_athene-userdebug
+add_lunch_combo carbon_athene-eng
